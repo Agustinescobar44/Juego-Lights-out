cf8cdf1570b82cb0b54ab3a85c55c3b1ca593fa5
agust <agust@192.168.0.14>
2021-08-24 01:51:16 -0300

    e

--- tp1_progra3/src/logica/lightsOut.java
+++ tp1_progra3/src/logica/lightsOut.java
@@ -12,7 +12,7 @@ public class lightsOut {
 		int contador= 1;
 		for (int i = 0; i < juego.tamanio(); i++) {
 			if(contador != 4){
-				juego.agregarArista(i, i+1);
+				juego.agregarArista(i, i+1); 
 				juego.agregarArista(i, i+4);
 				contador++;  
 			}
