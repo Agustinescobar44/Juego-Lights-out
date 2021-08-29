23deae5ee53f06846a502e37f5858e28e65e55b2
agust <agust@192.168.0.14>
2021-08-28 21:24:45 -0300

    cambiado aspecto y fondo en los botones

--- tp1_progra3/src/interfaz/App.java
+++ tp1_progra3/src/interfaz/App.java
@@ -10,10 +10,12 @@ import java.awt.GridLayout;
 import java.awt.Image;
 
 import javax.swing.JButton;
+import javax.naming.directory.AttributeModificationException;
 import javax.swing.AbstractButton;
 import javax.swing.ImageIcon;
 
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
@@ -35,7 +37,7 @@ public class App {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					App window = new App(3);
+					App window = new App(7);
 					window.frmLightsOut.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -63,7 +65,14 @@ public class App {
 		frmLightsOut = new JFrame();
 		frmLightsOut.setIconImage(Toolkit.getDefaultToolkit().getImage(App.class.getResource("/imagenes/icono_lights_out.png")));
 		frmLightsOut.setTitle("Lights Out!");
-		frmLightsOut.setBounds(100, 100, 800, 600);
+		
+		//obtengo las dimensiones de la pantalla para posicionar la ventana
+		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+		int ancho = 800;
+		int alto = 600;
+		int x = (pantalla.width/2) - (ancho/2);
+		int y = (pantalla.height/2) - (alto/2);
+		frmLightsOut.setBounds(x, y, ancho, alto);
 		frmLightsOut.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmLightsOut.getContentPane().setLayout(new GridLayout(0, i, 2, 2));
 		
@@ -71,20 +80,23 @@ public class App {
 		
 		for (int j = 0; j < i*i; j++) {
 			JButton temp = new JButton("");
-			agregarActionListener(temp, j);
+			temp.setContentAreaFilled(false);
+			temp.setFocusPainted(false);
+			temp.setOpaque(false);
+			agregarActionListener(temp, j , ancho , alto , i);
 			botones.add(temp);
 			frmLightsOut.getContentPane().add(temp);
 		}
 		
-		verLuces(frmLightsOut);
+		verLuces(frmLightsOut , ancho , alto , i);
 		
 	}
 	
-	private void agregarActionListener(JButton boton, int i ) {
+	private void agregarActionListener(JButton boton, int i , int ancho , int alto , int cantidad) {
 		boton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				juego.cambiarLuces(i);
-				verLuces(frmLightsOut);
+				verLuces(frmLightsOut , ancho , alto , cantidad);
 				if(juego.isGanador()) {
 					frmLightsOut.dispose();
 				}
@@ -92,19 +104,28 @@ public class App {
 		});
 	}
 	
-	private void verLuces(JFrame frame) {
+	private void verLuces(JFrame frame , int ancho , int alto, int cantidad) {
 		
 		Component[] componentes= frame.getContentPane().getComponents();
+		
+		//uso las imageicon como image para poder reescalarlas
 		Image luzprendidaimage = luzPrendidaIcono.getImage();
 		Image luzapagadaImage = luzApagadaIcono.getImage();
+		
+		ancho = ancho / cantidad -15;
+		alto = alto / cantidad -15;
+		
+		//reescalo las imagenes basado en el ancho y alto de la pantalla
+		Image luzprendidaCambiada = luzprendidaimage.getScaledInstance(ancho, alto, Image.SCALE_SMOOTH);
+		Image luzapagadaCambiada = luzapagadaImage.getScaledInstance(ancho, alto, Image.SCALE_SMOOTH);
 		
 		for (int i = 0; i < componentes.length; i++) {
 			if(componentes[i] instanceof JButton) {
 				if(juego.estaPrendida(i)) {
-					Image luzprendidaCambiada = luzprendidaimage.getScaledInstance(frame.getWidth()/4, frame.getHeight()/4, Image.SCALE_SMOOTH);
+					
 					((AbstractButton) componentes[i]).setIcon(new ImageIcon(luzprendidaCambiada));
 				}else {
-					Image luzapagadaCambiada = luzapagadaImage.getScaledInstance(frame.getWidth()/4, frame.getHeight()/4, Image.SCALE_SMOOTH);
+					
 					((AbstractButton) componentes[i]).setIcon(new ImageIcon(luzapagadaCambiada));
 				}
 					
